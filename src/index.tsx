a1eb90ebbf24bc1336e14f84d4ebe8da8f54a54b
David Rouyer <rouyer.david@gmail.com>
2023-04-28 21:52:38 +0200

    feat: organize imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import { App } from 'components/App';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+import { App } from 'components/App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
