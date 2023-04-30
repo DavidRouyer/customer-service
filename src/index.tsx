30d1331730dce280ecec959303fa797617567730
David Rouyer <rouyer.david@gmail.com>
2023-04-30 18:40:25 +0200

    feat: configure TypeScript paths

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { App } from 'components/App';
+import { App } from '@/components/App';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
