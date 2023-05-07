60d3b5583c95d113a7c7fc0c6c1ef0b2e20f5e69
David Rouyer <rouyer.david@gmail.com>
2023-05-07 19:02:26 +0200

    feat: messages

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -64,24 +64,48 @@ export const MessageList: FC = () => {
           >
             <div
               className={cn(
-                'order-2 mx-2 flex max-w-xs flex-col space-y-2 text-xs',
+                'order-2 mx-2 flex max-w-xs flex-col space-y-2 text-sm',
                 message.user.name === 'Tom Cook' && 'items-start',
                 message.user.name !== 'Tom Cook' && 'items-end'
               )}
             >
-              <div>
-                <span
-                  className={cn(
-                    'inline-block rounded-lg px-4 py-2',
-                    message.user.name === 'Tom Cook' &&
-                      'rounded-bl-none bg-gray-300 text-gray-600',
-                    message.user.name !== 'Tom Cook' &&
-                      'rounded-br-none bg-blue-600 text-white'
-                  )}
-                >
-                  {message.content}
-                </span>
-              </div>
+              {typeof message.content === 'string' && (
+                <div>
+                  <span
+                    className={cn(
+                      'inline-block rounded-lg px-4 py-2',
+                      message.user.name === 'Tom Cook' &&
+                        'rounded-bl-none bg-gray-300 text-gray-600',
+                      message.user.name !== 'Tom Cook' &&
+                        'rounded-br-none bg-blue-600 text-white'
+                    )}
+                  >
+                    {message.content}
+                  </span>
+                </div>
+              )}
+              {typeof message.content !== 'string' &&
+                message.content.map((content, index) => (
+                  <div key={content}>
+                    <span
+                      className={cn(
+                        'inline-block rounded-lg px-4 py-2',
+                        message.user.name === 'Tom Cook' &&
+                          'bg-gray-300 text-gray-600',
+                        message.user.name !== 'Tom Cook' &&
+                          'bg-blue-600 text-white',
+                        message.user.name === 'Tom Cook' &&
+                          index === message.content.length - 1 &&
+                          'rounded-bl-none',
+                        message.user.name !== 'Tom Cook' &&
+                          index === message.content.length - 1 &&
+                          'rounded-br-none'
+                      )}
+                    >
+                      {content}
+                    </span>
+                  </div>
+                ))}
             </div>
             <img
               src={message.user.imageUrl}
