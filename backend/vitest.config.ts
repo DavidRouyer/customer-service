58681a805df9c2adb424c75145d7a3cd14980427
David Rouyer <rouyer.david@gmail.com>
2023-05-09 22:55:30 +0200

    feat: init backend

---
+++ backend/vitest.config.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+// https://vitest.dev/config
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      reporter: ['lcov'],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
