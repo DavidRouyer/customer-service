73e91cf1230ec83efb6d18261823e0776b0bac78
David Rouyer <rouyer.david@gmail.com>
2023-06-24 18:17:33 +0200

    refactor: monorepo

--- apps/frontend/src/components/ui/Sheet.tsx
+++ apps/frontend/src/components/ui/Sheet.tsx
@@ -42,7 +42,7 @@ SheetPortal.displayName = SheetPrimitive.Portal.displayName;
 const SheetOverlay = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
->(({ className, children, ...props }, ref) => (
+>(({ className, ...props }, ref) => (
   <SheetPrimitive.Overlay
     className={cn(
       'fixed inset-0 z-50 bg-background/80 backdrop-blur-sm transition-all duration-100 data-[state=closed]:animate-out data-[state=closed]:fade-out data-[state=open]:fade-in',
@@ -55,14 +55,14 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  'fixed z-50 scale-100 gap-4 border bg-background p-6 opacity-100 shadow-lg',
+  'bg-background fixed z-50 scale-100 gap-4 border p-6 opacity-100 shadow-lg',
   {
     variants: {
       position: {
-        top: 'w-full animate-in slide-in-from-top duration-300',
-        bottom: 'w-full animate-in slide-in-from-bottom duration-300',
-        left: 'h-full animate-in slide-in-from-left duration-300',
-        right: 'h-full animate-in slide-in-from-right duration-300',
+        top: 'animate-in slide-in-from-top w-full duration-300',
+        bottom: 'animate-in slide-in-from-bottom w-full duration-300',
+        left: 'animate-in slide-in-from-left h-full duration-300',
+        right: 'animate-in slide-in-from-right h-full duration-300',
       },
       size: {
         content: '',
@@ -158,7 +158,8 @@ const SheetContent = React.forwardRef<
       {...props}
     >
       {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+      {/*eslint-disable-next-line tailwindcss/no-custom-classname*/}
+      <SheetPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
--- apps/frontend/src/hooks/useTicket/Message.ts
+++ apps/frontend/src/hooks/useTicket/Message.ts
@@ -8,6 +8,7 @@ import {
 export type Message = {
   id: string;
   createdAt: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   content: any;
   contentType: MessageContentType;
   direction: MessageDirection;
