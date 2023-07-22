72ae47148cc4a2fe2a1ee597d5511aef91378894
David Rouyer <rouyer.david@gmail.com>
2023-07-22 10:12:49 +0200

    refactor: extract `TicketHeader`

--- apps/frontend/src/app/Ticket.tsx
+++ apps/frontend/src/app/Ticket.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 
 import { MessageForm } from '@/components/MessageForm/MessageForm';
 import { MessageList } from '@/components/MessageList/MessageList';
+import { TicketHeader } from '@/components/TicketHeader/TicketHeader';
 import { useTicket } from '@/hooks/useTicket/TicketProvider';
 
 export const Component: FC = () => {
@@ -11,12 +12,7 @@ export const Component: FC = () => {
 
   return (
     <>
-      <div className="border-b border-gray-200 pb-5">
-        <h3 className="text-base font-semibold leading-6 text-gray-900">
-          <span className="text-gray-500">#{activeTicket.id}</span>{' '}
-          {activeTicket.contact.name}
-        </h3>
-      </div>
+      <TicketHeader />
 
       <MessageList />
       <MessageForm />
---
+++ apps/frontend/src/components/TicketHeader/TicketHeader.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+
+import { useTicket } from '@/hooks/useTicket/TicketProvider';
+
+export const TicketHeader: FC = () => {
+  const { activeTicket } = useTicket();
+
+  if (!activeTicket) return null;
+
+  return (
+    <div className="border-b border-gray-200 pb-5">
+      <h3 className="text-base font-semibold leading-6 text-gray-900">
+        <span className="text-gray-500">#{activeTicket.id}</span>{' '}
+        {activeTicket.contact.name}
+      </h3>
+    </div>
+  );
+};
+
+TicketHeader.displayName = 'TicketHeader';
