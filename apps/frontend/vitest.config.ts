facbb576bae08dacd5a3745fe9fc294b23912934
David Rouyer <rouyer.david@gmail.com>
2023-08-15 11:01:48 +0200

    build(deps-dev): bump @testing-library/jest-dom from 5.17.0 to 6.0.0

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    setupFiles: '.vitest/setup',
     environment: 'jsdom',
     environmentOptions: {
       jsdom: {
