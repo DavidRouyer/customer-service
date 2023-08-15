a8c65a8e5460497082d8ebfee55045a2bef9e772
David Rouyer <rouyer.david@gmail.com>
2023-08-15 11:10:26 +0200

    fix: tests

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    setupFiles: '.vitest/setup',
     environment: 'jsdom',
     environmentOptions: {
       jsdom: {
