8af3ab4610c1d53166d0e8cb3396aa54deaf075f
David Rouyer <rouyer.david@gmail.com>
2023-09-10 16:51:39 +0200

    chore: cleanup

--- apps/customer-service/src/components/sign-in.tsx
+++ apps/customer-service/src/components/sign-in.tsx
@@ -19,7 +19,7 @@ export function SignIn() {
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <button
-          onClick={() => signIn('github')}
+          onClick={() => signIn('github', { callbackUrl: '/tickets' })}
           className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           <FormattedMessage id="sign_in_with_github" />
