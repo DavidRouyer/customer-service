91ab2bc989cc4188804081e6a77c87124a28b1a9
David Rouyer <rouyer.david@gmail.com>
2023-09-13 11:43:20 +0200

    fix: auth

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -3,7 +3,7 @@ import type { DefaultSession } from '@auth/core/types';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import NextAuth from 'next-auth';
 
-import { db, eq, schema } from '@cs/database';
+import { db, eq, schema, tableCreator } from '@cs/database';
 import { sessions, users } from '@cs/database/schema/auth';
 
 export type { Session } from 'next-auth';
@@ -32,7 +32,7 @@ export const {
   CSRF_experimental,
 } = NextAuth({
   adapter: {
-    ...DrizzleAdapter(db),
+    ...DrizzleAdapter(db, tableCreator),
     async getSessionAndUser(data) {
       return (await db
         .select({
