bc635ae4ef66c84fc75508f8cdc57eacd113814e
David Rouyer <rouyer.david@gmail.com>
2023-09-21 12:09:44 +0200

    feat: fallbacks

--- apps/customer-service/src/app/tickets/[id]/page.tsx
+++ apps/customer-service/src/app/tickets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import { redirect } from 'next/navigation';
+import { Loader2 } from 'lucide-react';
 
 import { auth } from '@cs/auth';
 
@@ -23,14 +24,28 @@ export default async function TicketPage({
 
   return (
     <>
-      <Suspense fallback={null}>
+      <Suspense
+        fallback={
+          <aside className="fixed inset-y-0 right-0 hidden w-96 overflow-y-auto border-l px-4 py-6 sm:px-6 xl:block">
+            <div className="flex h-full w-full items-center justify-center">
+              <Loader2 className="h-8 w-8 animate-spin" />
+            </div>
+          </aside>
+        }
+      >
         <InfoPanel ticketId={ticketId} />
       </Suspense>
 
       <main className="lg:pl-60">
         <div className="xl:mr-96 xl:h-[100dvh] xl:overflow-y-auto xl:pl-96">
           <div className="flex h-[100dvh] flex-col px-4 py-6 sm:px-6">
-            <Suspense fallback={null}>
+            <Suspense
+              fallback={
+                <div className="flex h-full w-full items-center justify-center">
+                  <Loader2 className="h-8 w-8 animate-spin" />
+                </div>
+              }
+            >
               <Ticket ticketId={ticketId} />
             </Suspense>
           </div>
