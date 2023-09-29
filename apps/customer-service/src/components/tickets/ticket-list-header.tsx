647e4f1d1045b469307bd849e28357b7de22931c
David Rouyer <rouyer.david@gmail.com>
2023-09-29 16:53:41 +0200

    fix: color

--- apps/customer-service/src/components/tickets/ticket-list-header.tsx
+++ apps/customer-service/src/components/tickets/ticket-list-header.tsx
@@ -6,7 +6,7 @@ export const TicketListHeader = () => {
 
   return (
     <header className="flex items-center justify-between border-b px-4 py-6 sm:px-6">
-      <h1 className="text-base font-semibold leading-10 text-white">
+      <h1 className="text-base font-semibold leading-10 text-foreground">
         {searchParams.get('filter') === 'me' ? (
           <FormattedMessage id="layout.tickets.my_tickets" />
         ) : searchParams.get('filter') === 'unassigned' ? (
