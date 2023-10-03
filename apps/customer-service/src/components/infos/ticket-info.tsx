9da7255c5e91c2a4b80181141e0ec6dfa719bf46
David Rouyer <rouyer.david@gmail.com>
2023-10-03 22:18:00 +0200

    chore: cleanup

--- apps/customer-service/src/components/infos/ticket-info.tsx
+++ apps/customer-service/src/components/infos/ticket-info.tsx
@@ -15,15 +15,15 @@ export const TicketInfo: FC<{ ticketId: number }> = ({ ticketId }) => {
   }
 
   return (
-    <dl className="grid grid-cols-[5rem,_1fr] items-center gap-x-3 gap-y-4 border-b py-4">
-      <dt className="text-sm leading-5">
+    <dl className="grid grid-cols-[5rem,_1fr] items-center gap-x-3 gap-y-2 border-b py-4">
+      <dt className="text-sm leading-8">
         <FormattedMessage id="info_panel.ticket_panel.status" />
       </dt>
       <dd className="truncate text-sm leading-5 text-muted-foreground">
         <TicketStatusDropdowm status={ticketData?.status} ticketId={ticketId} />
       </dd>
 
-      <dt className="text-sm leading-5">
+      <dt className="text-sm leading-8">
         <FormattedMessage id="info_panel.ticket_panel.assignee" />
       </dt>
       <dd className="truncate text-sm leading-5 text-muted-foreground">
