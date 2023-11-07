4c2fcaff7c0ad603ed90dc94c5db37aab31f5afb
David Rouyer <rouyer.david@gmail.com>
2023-11-07 22:04:54 +0100

    fix(vercel): fix url

--- apps/customer-service/src/app/providers.tsx
+++ apps/customer-service/src/app/providers.tsx
@@ -13,7 +13,7 @@ import { api } from '~/lib/api';
 
 const getBaseUrl = () => {
   if (typeof window !== 'undefined') return ''; // browser should use relative url
-  if (process.env.VERCEL_URL) return process.env.VERCEL_URL; // SSR should use vercel url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
   return `http://localhost:${process.env.PORT}`; // dev SSR should use localhost
 };
