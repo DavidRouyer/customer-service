48f344f884a1929d17f9a88e6b19e7d080894b81
David Rouyer <rouyer.david@gmail.com>
2023-11-21 22:26:01 +0100

    fix: build

--- packages/lib/generate-entity-id.ts
+++ packages/lib/generate-entity-id.ts
@@ -1,4 +1,12 @@
-import { ulid } from 'ulid';
+import { factory } from 'ulid';
+
+// https://github.com/ulid/javascript/issues/72#issuecomment-1143617380
+const prng = () => {
+  const buffer = new Uint8Array(1);
+  crypto.getRandomValues(buffer);
+  return buffer[0] ?? 0 / 0xff;
+};
+const ulid = factory(prng);
 
 /**
  * Generate a composed id based on the input parameters and return either the is if it exists or the generated one.
