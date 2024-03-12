6f387128b47f2f7a87641f581fbe19cde2866683
David Rouyer <rouyer.david@gmail.com>
2024-03-12 22:28:24 +0100

    fix: config

--- tooling/tailwind/index.ts
+++ tooling/tailwind/index.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import animate from 'tailwindcss-animate';
 
 export default {
   content: [''],
@@ -75,5 +76,5 @@ export default {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [animate],
 } satisfies Config;
