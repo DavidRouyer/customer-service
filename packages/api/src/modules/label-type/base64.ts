1d38053175e5a61ffd0c3788cfefb94879c549ac
David Rouyer <rouyer.david@gmail.com>
2024-04-11 23:01:58 +0200

    feat: add tests

--- packages/api/src/modules/label-type/base64.ts
+++ packages/api/src/modules/label-type/base64.ts
@@ -1,5 +1,5 @@
 export const base64 = (str: string) =>
-  Buffer.from(JSON.stringify(str), 'utf8').toString('base64');
+  Buffer.from(str, 'utf8').toString('base64');
 
 export const unbase64 = (str: string) =>
   Buffer.from(str, 'base64').toString('utf8');
