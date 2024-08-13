10943867be6d8592ed46321eb198df908dec66fa
David Rouyer <rouyer.david@gmail.com>
2024-08-13 23:23:56 +0200

    fix: nitro deployment bug

--- apps/customer-service/app.config.ts
+++ apps/customer-service/app.config.ts
@@ -16,7 +16,7 @@ export default createApp({
     },
     // Fixes https://github.com/nuxt/nuxt/issues/27784
     externals: {
-      traceInclude: ['node_modules/lexical/Lexical.prod.mjs'],
+      traceInclude: ['node_modules/@lexical/headless/LexicalHeadless.node.mjs'],
     },
   },
   routers: [
