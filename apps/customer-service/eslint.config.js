48a321a5b582fe4cfef4995abdbcecc79ad0601f
David Rouyer <rouyer.david@gmail.com>
2024-09-08 11:35:50 +0200

    chore: cleanup

--- apps/customer-service/eslint.config.js
+++ apps/customer-service/eslint.config.js
@@ -5,7 +5,11 @@ import tailwindConfig from '@cs/eslint-config/tailwind';
 /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigArray} */
 export default [
   {
-    ignores: ['.vinxi/**', 'src/graphql/generated/client.ts'],
+    ignores: [
+      '.vinxi/**',
+      'app/routeTree.gen.ts',
+      'app/graphql/generated/client.ts',
+    ],
   },
   ...baseConfig,
   ...reactConfig,
